f38092f76a72592497b862081723b0ade5d8d412
Taiki Yamanaka <tyamanakatyamanaka@gmail.com>
2024-03-08 19:07:24 +0900

    change to correct flight data as localfile at temporaly

--- anemometer_server/flightdata/views.py
+++ anemometer_server/flightdata/views.py
@@ -11,12 +11,23 @@ from rest_framework.views import APIView
 url="https://optimalis-database-default-rtdb.asia-southeast1.firebasedatabase.app/.json"
 sess=requests.session()
 
+getdata_filepath="./getdata.json"
+
 
 class LatestData():
     LHWD=[]#WindSpeed:,Time:(datetime型),AID
 
-    def updateLHWD(self,givendata): 
-        self.LHWD.append(givendata)
+    def updateLHWD(self): 
+        f=open(getdata_filepath,mode='r')
+        givendata=[]
+        for item in f.readlines():
+            givendata.append(json.loads(item))
+        f.close()
+        f=open(getdata_filepath,mode='w')
+        f.close()
+        for item in givendata:
+            item['Time']=datetime.datetime.strptime(item['Time'],'%Y-%m-%d %H:%M:%S.%f')
+            self.LHWD.append(item)
     
     def checkLHWD(self):
         rmlist=[]
@@ -28,31 +39,39 @@ class LatestData():
     
 
 fetch_fd=False
+fetch_fd=True
 latestdata=LatestData()
 
 def get():
+    print("start to getting")
     if not fetch_fd:
         return 0
     get=sess.get(url) 
     json_data=json.loads(get.text)
-    json_data['Time']=datetime.datetime.now()
-    latestdata.updateLHWD(json_data)
+    json_data['Time']=str(datetime.datetime.now())
+    f=open(getdata_filepath,mode='a')
+    f.write(json.dumps(json_data)+"\n")
+    f.close()
+    print("update")
+    
 
 def start():
    scheduler=BackgroundScheduler()
-
-   scheduler.add_job(get,'interval',seconds=0.5)
+   scheduler.add_job(get,'interval',seconds=2)
    scheduler.start()
+   print("flight data get is scheduled")
 
 
 
 class LHWD(APIView):
     def get(self,response):
+        latestdata.updateLHWD()
         latestdata.checkLHWD()
         return Response(latestdata.LHWD)
 
 class LD(APIView):
     def get(self,response):
+        latestdata.updateLHWD()
         if len(latestdata.LHWD) == 0:
             return Response([])
         ld_last=latestdata.LHWD[0]
