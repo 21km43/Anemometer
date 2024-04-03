a40d3924298fe6b51ee0ef73b1c49302b02308c9
root <root@localhost.localdomain>
2024-04-03 10:19:30 -0400

    mod

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ docker compose up -d
 chmod +x anemometer_server/manage.py
 
 #DB,staticファイルの再構築
+docker compose exec django chmod +x ./manage.py
 docker compose exec django ./manage.py makemigrations
 docker compose exec django ./manage.py migrate
 docker compose exec django ./manage.py collectstatic --noinput
