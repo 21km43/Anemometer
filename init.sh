e6eb0328d45a4b9b79e2af37c7f267e402e8ee5d
root <root@localhost.localdomain>
2024-04-05 05:08:42 -0400

    add grafana setting

--- init.sh
+++ init.sh
@@ -2,8 +2,6 @@
 docker compose build
 docker compose up -d
 
-chmod +x anemometer_server/manage.py
-
 #DB,staticファイルの再構築
 docker compose exec django chmod +x ./manage.py
 docker compose exec django ./manage.py makemigrations
